340fa6b4e6b54d5bea27e5008d58674613170bee
ray <reirei256@gmail.com>
2021-08-16 01:37:11 +0200

    Pretty time

--- src/main/java/com/forum/model/Post.java
+++ src/main/java/com/forum/model/Post.java
@@ -3,11 +3,15 @@ package com.forum.model;
 import com.forum.model.audit.DateAudit;
 import com.forum.util.SlugUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.Locale;
 
 @Setter
 @Getter
@@ -37,6 +41,12 @@ public class Post extends DateAudit {
 
     @ManyToOne
     private Category category;
+
+    public String getPostedTime() {
+        Locale locale = LocaleContextHolder.getLocale();
+        PrettyTime p = new PrettyTime(locale);
+        return p.format(Date.from(getCreatedAt()));
+    }
 
     @PrePersist
     public void slugColumn() {

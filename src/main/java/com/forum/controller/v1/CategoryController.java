5475f6771b2aad743f89abebc9ad774c74085338
ray <reirei256@gmail.com>
2021-08-21 05:09:03 +0200

    Category controller test

--- src/main/java/com/forum/controller/v1/CategoryController.java
+++ src/main/java/com/forum/controller/v1/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
---
+++ src/test/java/com/forum/controller/v1/CategoryControllerTest.java
@@ -0,0 +1,68 @@
+package com.forum.controller.v1;
+
+import com.forum.model.Category;
+import com.forum.model.Post;
+import com.forum.service.CategoryService;
+import com.forum.service.PostService;
+import com.forum.service.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = CategoryController.class)
+@ExtendWith(MockitoExtension.class)
+class CategoryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+    @MockBean
+    private CategoryService categoryService;
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Test
+    void shouldGetCategoryBySlug() throws Exception {
+        Category category = Category.builder()
+                .id(1L)
+                .name("name")
+                .description("desc")
+                .slug("name")
+                .posts(new ArrayList<>())
+                .build();
+        when(categoryService.getCategoryBySlug(any())).thenReturn(Optional.of(category));
+        when(postService.getAllByCategory(any())).thenReturn(Optional.of(new ArrayList<Post>()));
+
+        mockMvc.perform(get("/category/name"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("category/category"))
+                .andExpect(model().attributeExists("category"))
+                .andExpect(model().attributeExists("posts"));
+    }
+
+    @Test
+    void shouldThrowNotFound() throws Exception {
+        when(categoryService.getCategoryBySlug(any())).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/category/name"))
+                .andExpect(status().isNotFound())
+                .andExpect(mvcResult -> assertTrue(mvcResult.getResolvedException() instanceof ResponseStatusException));
+    }
+}
